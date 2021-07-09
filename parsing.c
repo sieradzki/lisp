e1621b98b529bd461e4ebc8713dfe9cc9bc3c851
sieradzki <sieradzki.mateusz@outlook.com>
2021-07-09 15:01:04 +0200

    chapter_6

--- parsing.c
+++ parsing.c
@@ -25,18 +25,19 @@ void add_history(char* unused) {}
 
 int main(int argc, char** argv){
   /*Create Some Parsers */
-  mpc_parser_t* Number   = mpc_new("number");
-  mpc_parser_t* Operator = mpc_new("operator");
-  mpc_parser_t* Expr     = mpc_new("expr");
-  mpc_parser_t* Lispy    = mpc_new("lispy");
+  mpc_parser_t* Number        = mpc_new("number");
+  mpc_parser_t* Operator      = mpc_new("operator");
+  mpc_parser_t* Expr          = mpc_new("expr");
+  mpc_parser_t* Lispy         = mpc_new("lispy");
   
   /* Define them with the following Language */
   mpca_lang(MPCA_LANG_DEFAULT,
-      "                                                   \
-      number    : /-?[0-9]+/ ;                            \
-      operator  : '+' | '-' | '*' | '/' ;                 \
-      expr      : <number> | '(' <operator> <expr>+ ')' ; \
-      lispy     : /^/ <operator> <expr>+ /$/ ;            \
+      "                                                         \
+      number       : /-?[0-9]+[.]*[0-9]*/ ;                               \
+      operator     : '+' | '-' | '*' | '/' | '%' |              \
+      \"add\" | \"sub\" | \"mult\" | \"div\";                   \
+      expr         : <number> | '(' <operator> <expr>+ ')' ;    \
+      lispy        : /^/ <operator> <expr>+ /$/ ;               \
       ",
       Number, Operator, Expr, Lispy);
 
